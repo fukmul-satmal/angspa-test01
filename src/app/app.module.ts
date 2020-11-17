d744a9dd8fb836265046d1d1dae6dc4304cb8745
fukmul-satmal <sato_rin_sato_imo@yahoo.co.jp>
2020-11-17 13:45:02 +0900

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 
+// Import the module from the SDK
+import { AuthModule } from '@auth0/auth0-angular';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AuthButtonComponent } from './auth-button/auth-button.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, UserProfileComponent, AuthButtonComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    // Import the module into the application, with configuration
+    AuthModule.forRoot({
+      domain: 'fukmul-satmal-02.auth0.com',
+      clientId: 'f2TFdb7QID12XZUwNW6mRO9AjdAEnFen',
+      redirectUri: `${window.location.origin}/angspa-test01/`
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
