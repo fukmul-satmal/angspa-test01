337a18d3b53214034e600a092f9836ca369afe1d
fukmul-satmal <sato_rin_sato_imo@yahoo.co.jp>
2020-12-03 18:41:22 +0900

    add API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
+// Interceptor
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthHttpInterceptor } from '@auth0/auth0-angular';
+import { environment as env } from '../environments/environment';
+
 // Import the module from the SDK
 import { AuthModule } from '@auth0/auth0-angular';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { AuthButtonComponent } from './auth-button/auth-button.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ExternalApiComponent } from './pages/external-api/external-api.component';
+
 
 @NgModule({
-  declarations: [AppComponent, UserProfileComponent, AuthButtonComponent],
+  declarations: [AppComponent, UserProfileComponent, AuthButtonComponent, ExternalApiComponent],
   imports: [
     BrowserModule,
 
@@ -17,10 +24,24 @@ import { AppRoutingModule } from './app-routing.module';
     AuthModule.forRoot({
       domain: 'fukmul-satmal-02.auth0.com',
       clientId: 'f2TFdb7QID12XZUwNW6mRO9AjdAEnFen',
-      redirectUri: `https://fukmul-satmal.github.io/angspa-test01/`
+      redirectUri: `https://fukmul-satmal.github.io/angspa-test01/`,
+
+      //アクセストークンをアタッチするInterceptorのURL設定
+      httpInterceptor: {
+        allowedList: [`${env.dev.serverUrl}/api/messages/protected-message`],
+      },
     }),
 
     AppRoutingModule,
+  ],
+
+  //AuthHttpInterceptorをプロバイダーとして登録
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthHttpInterceptor,
+      multi: true,
+    },
   ],
 
   bootstrap: [AppComponent],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
+
+import { domain, clientId, audience, serverUrl } from '../../auth_config.json';
+
 export const environment = {
-  production: true
+  production: true,
+  auth: {
+    domain,
+    clientId,
+    redirectUri: window.location.origin,
+    audience,
+  },
+  dev: {
+    serverUrl,
+  }
 };
