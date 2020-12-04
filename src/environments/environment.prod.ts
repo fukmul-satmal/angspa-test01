6f2294def8c4f242445d7d5316d1bdf56527385d
fukmul-satmal <sato_rin_sato_imo@yahoo.co.jp>
2020-12-04 19:00:37 +0900

    edit auth_config.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 
-//import { domain, clientId, audience, serverUrl } from '../../auth_config.json';
-import { domain, clientId, serverUrl } from '../../auth_config.json';
+import { domain, clientId, audience, serverUrl } from '../../auth_config.json';
 
 export const environment = {
   production: true,
@@ -8,7 +7,7 @@ export const environment = {
     domain,
     clientId,
     redirectUri: window.location.origin,
-//    audience,
+    audience,
   },
   dev: {
     serverUrl,
