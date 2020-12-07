7c10fadb6cf639f670f43651cf79f1a377ac24f8
fukmul-satmal <sato_rin_sato_imo@yahoo.co.jp>
2020-12-07 13:44:05 +0900

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { ExternalApiComponent } from './pages/external-api/external-api.component';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '@auth0/auth0-angular';
 
 // ルーティングの設定
 const routes: Routes = [
-  { path: 'user-profile', component: UserProfileComponent, canActivate: [AuthGuard] }
+  { path: 'user-profile', component: UserProfileComponent, canActivate: [AuthGuard] },
+  { path: 'external-api', component: ExternalApiComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
