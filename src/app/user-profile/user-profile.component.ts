1b5ec8d1ca26c477144fa9cf06b08c188d86b124
fukmul-satmal <sato_rin_sato_imo@yahoo.co.jp>
2021-02-12 16:53:01 +0900

    add AuthGuard

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
-  selector: 'user-profile',
+  selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
