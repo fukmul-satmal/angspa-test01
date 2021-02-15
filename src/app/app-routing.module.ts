45aedd7c2e8db626786eab20ac3445a26fae1727
fukmul-satmal <sato_rin_sato_imo@yahoo.co.jp>
2021-02-15 15:20:51 +0900

    add AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import { AuthGuard } from '@auth0/auth0-angular';
 
 // ルーティングの設定
 const routes: Routes = [
-//  { path: 'user-profile', component: UserProfileComponent, canActivate: [AuthGuard], outlet: 'UserProfile' },
-  { path: 'user-profile', component: UserProfileComponent, canActivate: [AuthGuard] },
+  { path: 'user-profile', component: UserProfileComponent, canActivate: [AuthGuard], outlet: 'UserProfile' },
+//  { path: 'user-profile', component: UserProfileComponent, canActivate: [AuthGuard] },
   { path: 'external-api', component: ExternalApiComponent, canActivate: [AuthGuard] }
 ];
 
